2218dc1f61ba048df3806324c3b5e505915acb93
Natalia Kniat <natalia.kniat@nokia.com>
2022-03-11 20:54:53 +0100

    zadania zdrowie draft

---
+++ zdrowie_app_draft.py
@@ -0,0 +1,10 @@
+# program liczy prawdopodobnieństwo, że pacjent będzie miał problemy z sercem
+# program bierze pod uwagę stres, ruch i dietę
+
+stres = poziomstresu(rodzaj_pracy, tiki)
+
+ruch = ileruchu(zwykly_ruch, ile_cwiczen)
+
+dieta = jakadieta(wege, tlusto)
+
+#wyliczenie ryzyka problemów z sercem
---
+++ zdrowie_funkcje_draft.py
@@ -0,0 +1,10 @@
+def poziomstresu(rodzaj_pracy, tiki_nerwowe):
+
+
+
+def ileruchu(ile_zwyklego_ruchu, ile_cwiczen):
+
+
+
+def jakadieta(czy_wege, czy_tlusto):
+
