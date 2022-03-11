1f9a7e91fa7ff884849f0f4834274314ac8cc88c
Natalia Kniat <natalia.kniat@nokia.com>
2022-03-11 20:46:07 +0100

    kosmetyka

--- zdrowie_app.py
+++ zdrowie_app.py
@@ -1,8 +1,8 @@
 # program liczy prawdopodobnieństwo, że pacjent będzie miał problemy z sercem
 # program bierze pod uwagę stres, ruch i dietę
-from funkcje import *
+from zdrowie import *
 
-rodzaj_pracy = int(input('Jaki masz rodzaj pracy?\n1.Zwykla\n2.zarządzanie\n3.Wysokiego ryzyka\n'))
+rodzaj_pracy = int(input('Jaki masz rodzaj pracy?\n1.Na produkcji\n2.Biurowa\n3.Wysokiego ryzyka\n'))
 tiki = input('Czy masz tiki nerwowe?  T/N   ')
 if tiki == "T" or tiki == "t" or tiki == "Tak":
     tiki = True
